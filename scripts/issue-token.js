e2db21ce0332367ba4e01daedcd8dd4f8e2a6086
Harshit singhal <91280922+supernovahs@users.noreply.github.com>
2022-01-01 14:17:24 +0530

    Add files via upload

---
+++ scripts/issue-token.js
@@ -0,0 +1,8 @@
+const TokenFarm = artifacts.require('TokenFarm')
+
+module.exports = async function(callback){
+    let tokenFarm = await TokenFarm.deployed()
+    await tokenFarm.issueToken()
+    console.log("tokens issued")
+    callback()
+}
